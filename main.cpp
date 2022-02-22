2352c4d7d875f76307133e05165dcd3ab1fc7280
iABF <437040468@qq.com>
2022-02-22 16:17:45 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<utility>
+#include<queue>
+#include<map>
+
+#define INF 0x7f7f7f7f
+#define MAXN 30020
+#define _PI 3.14159265358979323846
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+}
